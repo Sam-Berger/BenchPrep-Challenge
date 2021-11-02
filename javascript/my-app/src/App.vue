906e87b3e58c6823b4d10a43e90118337e6bc021
Sam-Berger <sam.d.berger@gmail.com>
2021-11-02 13:00:03 -0500

    paginated data

--- javascript/my-app/src/App.vue
+++ javascript/my-app/src/App.vue
@@ -1,24 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <Quotes msg = "Quote imbed message">
     </Quotes>
-    <Practice msg="fdsdfsdfsd"/>
-    <HelloWorld msg="Welcome to Your Vue.js AppIII"/>
+    <!-- <Practice msg="fdsdfsdfsd"/> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js AppIII"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Practice from './components/Practice.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import Practice from './components/Practice.vue'
 import Quotes from './components/Quotes.vue'
 // import quoteData from 'https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/quotes.json'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    Practice,
+    // HelloWorld,
+    // Practice,
     Quotes
   }
 }
--- javascript/my-app/src/components/Quotes.vue
+++ javascript/my-app/src/components/Quotes.vue
@@ -1,22 +1,30 @@
 
 <template>
   <div class="quotes">
-    <!-- <h1>{{ msg }}</h1> -->
-    <!-- <h2>{{objectExample}}</h2>
+      <div>
+        <h2>Quotes!!!</h2>
+        <button v-on:click="showMovieQuotes()">Show Only Movie Quotes</button>
+        <button v-on:click="showGameQuotes()">Show Only Game Quotes</button>
 
-    <h2>{{objectExample.qwe}}</h2> -->
-    <h2>Quotes!!!</h2>
-    <ul>
-        <div v-for="item in quotesData" v-bind:key="item">
-            <h1>{{item.quote}}</h1>
-            <h2>{{item.source}}</h2>
-            <h3>{{item.context}}</h3>
-            <h4>{{item.theme}}</h4>
-        </div>
-    </ul>
-
-    <h2>{{quotesData[0]}}</h2>
-    <p>QUOTES END</p>
+        <!-- <h1>Page {{page}}</h1> -->
+        <!-- <h1>Per Page {{perPage}}</h1> -->
+        <!-- <h1>quotesLength {{quotesData.length}}</h1>
+        <h1>paginatedquotesLength {{paginatedQuotes.length}}</h1> -->
+        <!-- <h1>paginatedquotesLength {{paginate(quotesData)}}</h1> -->
+        <!-- <h1>paginatedquotesLength {{(paginatedQuotes)}}</h1> -->
+        <ul>
+            <div v-for="item in paginatedQuotes" v-bind:key="item">
+                <h1>{{item.quote}}</h1>
+                <h2>{{item.source}}</h2>
+                <h3>{{item.context}}</h3>
+                <h4>{{item.theme}}</h4>
+            </div>
+        </ul>
+      </div>
+      <div class="pagination-nav">
+          <button v-on:click="pageDown()" v-if="page>1">Prev</button>
+          <button v-on:click="pageUp()" v-if="page <pages">Next</button>
+      </div>
   </div>
 </template>
 
@@ -28,22 +36,57 @@ export default {
   },
   data: function () {
     return {
-      foo: 'bar',
-      objectExample: {
-          qwe: "wer",
-          tre: "www"
-      },
-      quotesData: []
+      quotesData: [],
+      activeQuotesData: [],
+      page: 1,
+      perPage: 15,
+      pages: 1
     }
+  },
+  methods:{
+      getData() {
+          fetch('https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/quotes.json')
+            .then(response => response.json())
+            .then(data => this.quotesData = data);
+      },
+      getNumberOfPages() {
+          this.pages = Math.ceil(this.quotesData.length/this.perPage)
+      },
+      paginate(quotesData) {
+          let from = (this.page - 1) * this.perPage
+          let to = this.page * this.perPage
+          return quotesData.slice(from,to)
+      },
+      pageUp() {
+          this.page++
+      },
+      pageDown(){
+          this.page--
+      },
+      showGameQuotes(){
+          return this.quotesData = this.quotesData.filter(quote => quote.theme == "games")
+      },
+      showMovieQuotes(){
+          return this.quotesData = this.quotesData.filter(quote => quote.theme == "movies")
+      }
+
+  },
+  computed: {
+	paginatedQuotes: function () {
+		return this.paginate(this.quotesData);
+	}
+},
+  watch: {
+      quotesData() {
+          this.getNumberOfPages()
+      }
   },
 
   created() {
-  // Simple GET request using fetch
-  fetch('https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/quotes.json')
-    .then(response => response.json())
-    .then(data => this.quotesData = data);
+    this.getData()
   }
 }
+
 
 //   created() {
 //   // Simple GET request using fetch
